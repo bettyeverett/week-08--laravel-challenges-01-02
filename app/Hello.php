9fa2999707470696ab59dd9b9ff3b6694bc33cc1
Bethany Everett <bethanyeeverett@gmail.com>
2019-11-19 13:00:24 +0000

    Creating a file structure to use composer package manager and auto loader and Task One: Creating a basic Hello class and a function to return Hello and a string.

---
+++ app/Hello.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+class Hello
+{
+
+    private $string;
+
+    public function hello($string)
+    {
+
+        return "Hello " . $string;
+
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+echo "\nQuestion 1:\n";
+
+use App\Hello;
+
+$sayHi = new Hello();
+
+dump($sayHi->hello("Orb")); // "Hello Orb"
+dump($sayHi->hello("Horse")); // "Hello Horse"
+
