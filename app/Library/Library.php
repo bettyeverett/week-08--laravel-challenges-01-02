cae0970f266a5416844ea4cb5cb4c1f0fb8da7ab
Bethany Everett <bethanyeeverett@gmail.com>
2019-11-20 10:32:12 +0000

    Task Six: Creating a Library class that draws from functionality of Shelf and therefore Book classes, to extract an array of book titles.

---
+++ app/Library/Library.php
@@ -0,0 +1,33 @@
+<?php
+
+// Create a class Library. It should have an addShelf() method, which takes a Shelf object. It should have a titles() method that lists all the titles of all the books on all the shelves in the library.
+
+namespace App\Library;
+
+class Library
+{
+
+    private $shelves = [];
+
+    public function addShelf(Shelf $shelf)
+    {
+
+        $this->shelves[] = $shelf;
+        return $this;
+
+    }
+
+    public function titles()
+    {
+
+        $titles = [];
+
+        foreach($this->shelves as $shelf) {
+            $titles[] = $shelf->titles();
+        }
+
+        return $titles;
+
+    }
+
+}
